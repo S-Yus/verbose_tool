ea04319404ec7d6e25626ce17eefe7c2ad9254c2
Yusei <sa2yusei1030@icloud.com>
2026-04-26 20:22:16 +0900

    Rename title to 政治家

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -48,7 +48,7 @@ export default function App() {
 
   return (
     <div className="container">
-      <h1 className="title">冗長くん</h1>
+      <h1 className="title">政治家</h1>
       <p className="subtitle">短い文章を無駄に長くします</p>
 
       <textarea
